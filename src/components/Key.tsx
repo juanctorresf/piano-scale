5996d8aef1df73995422083f6357affce9a021f3
Juan Torres <jtorres.devv@gmail.com>
2024-04-24 12:39:31 -0500

    Add custom class to the piano using clsx

--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -5,20 +5,21 @@ interface PrompDataKey {
 	// isPlaying: boolean
 }
 
-export const Key: React.FC<PrompDataKey> = ({ note }) => {
+export const KeyPiano: React.FC<PrompDataKey> = ({ note }) => {
+
 	return (
 		<div 
-			className={ clsx(
-				"w-5 h-20 text-center bg-white border-2 border-solid my-class border-gray-10",
-				"lg:w-10 lg:h-52",
-				"hover:bg-gray-400 hover:cursor-pointer"
-			)}
+			className={ clsx({
+				"white-key" : note.length <= 2,
+				"black-key" : note.length > 2,
+				})
+			}
 		>
 			<div className={ clsx(
-				"relative mt-16 text-sm text-black uppercase",
-				"lg:text-base lg:mt-44")
-			}>
-				{ note }
+				"relative mt-20 text-sm text-black uppercase",
+				"lg:text-base lg:mt-44"
+			)}>
+				{ note.length <= 2 ? note : null }
 			</div>
 		</div>
 	)
