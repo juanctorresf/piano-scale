c1a539b10a42e08cefe03ed7657a05bb7ad9c90e
Juan Torres <jtorres.devv@gmail.com>
2024-04-25 18:16:45 -0500

    Add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,14 @@
-import { useScaleStore } from "./store/scaleStore"
-import clsx from "clsx";
-
-import { InputModality } from "./components/InputModality";
-import { InputNote } from "./components/InputNote";
-import { InputType } from "./components/InputType";
-
-import { modality, notes, typeMajors,typeMinors } from "./global/ScaleInfo"
-import { KeyPiano } from "./components/Key";
-import { keys as keys } from "./global/KeyData";
+import { Piano } from "./components/Piano";
+import { Selectors } from "./components/Selectors";
+import { Header } from "./components/Header";
 
 
 export const App = () => {
-	const modalityStore = useScaleStore( state => state.modality );    
-
 	return (
 		<main>
-			<h1 
-				className={ clsx(
-					"m-0 text-5xl text-center text-gray-10 pt-21 mb-9",
-					"md:text-6xl md:pt-45"
-				)}
-			>
-				MusicScale
-			</h1>
-
-			<div 
-				className={clsx(
-					"flex flex-col items-center max-w-5xl ",
-					"lg:flex-row lg:mx-auto"
-				)
-			}>
-				<InputNote inputNote={ notes } labelName="Tone"/>
-				<InputModality inputModality={ modality } labelName="Modality" /> 
-				{ modalityStore === "Major" && <InputType inputType={ typeMajors } labelName="Type"/> }
-				{ modalityStore === "Minor" && <InputType inputType={ typeMinors } labelName="Type"/> }
-			</div>
-
-			<div className="flex flex-row justify-center my-7">
-				{
-					keys.map( key => (
-						<KeyPiano key={ key } note={ key } />
-					))
-				}
-			</div>
+			<Header />
+			<Selectors />
+			<Piano />
 		</main>
 	)
 }
