f145efb966322d260e1eb60df0939b0b6b922dcf
Juan Torres <jtorres.devv@gmail.com>
2024-04-25 18:17:39 -0500

    Add Selectors like a component

---
+++ src/components/Selectors.tsx
@@ -0,0 +1,25 @@
+import clsx from "clsx"
+import { modality, notes, typeMajors, typeMinors } from "../global/ScaleInfo"
+import { InputModality } from "./InputModality"
+import { InputNote } from "./InputNote"
+import { InputType } from "./InputType"
+import { useScaleStore } from "../store/scaleStore"
+
+
+export const Selectors = () => {
+    const modalityStore = useScaleStore( state => state.modality );
+    
+    return (
+        <div 
+            className={clsx(
+                "flex flex-col items-center max-w-5xl ",
+                "lg:flex-row lg:mx-auto"
+            )
+        }>
+            <InputNote inputNote={ notes } labelName="Tone"/>
+            <InputModality inputModality={ modality } labelName="Modality" /> 
+            { modalityStore === "Major" && <InputType inputType={ typeMajors } labelName="Type"/> }
+            { modalityStore === "Minor" && <InputType inputType={ typeMinors } labelName="Type"/> }
+        </div>
+    )
+}
