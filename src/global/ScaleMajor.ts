faf39c837d7fa2f303210541db8dcba3d58aa005
Juan Torres <jtorres.devv@gmail.com>
2024-04-29 22:19:05 -0500

    Add major ionian chords

---
+++ src/global/ScaleMajor.ts
@@ -0,0 +1,27 @@
+const cMajorIonian: string[] = [ "C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5" ];
+const cSharpMajorIonian: string[] = [ "C#4", "D#4", "F", "F#4", "G#4", "A#4", "C5", "C#5" ];
+const dMajorIonian: string[] = [ "D4", "E4", "F#4", "G4", "A4", "B4", "C#5", "D5" ];
+const dSharpMajorIonian: string[] = [ "D#4", "F4", "G4", "G#4", "A#4", "C5", "D5", "D#5" ];
+const eMajorIonian: string[] = [ "E4", "F#4", "G#4", "A4", "B4", "C#5", "D#5", "E5" ];
+const fMajorIonian: string[] = [ "F4", "G4", "A4", "A#4", "C5", "D5", "E5", "F5" ];
+const fSharpMajorIonian: string[] = [ "F#4", "G#4", "A#4", "B4", "C#5", "D#5", "F5", "F#5" ];
+const gMajorIonian: string[] = [ "G4", "A4", "B4", "C5", "D5", "E5", "F#5", "G5" ];
+const gSharpMajorIonian: string[] = [ "G#4", "A#4", "C5", "C#5", "D#5", "F5", "G5", "G#5" ];
+const aMajorIonian: string[] = [ "A4", "B4", "C#5", "D5", "E5", "F#5", "G#5", "A5" ];
+const aSharpMajorIonian: string[] = [ "A#4", "C5", "D5", "D#5", "F5", "G5", "A5", "A#5" ];
+const bMajorIonian: string[] = [ "B4", "C#5", "D#5", "E5", "F#5", "G#5", "A#5", "B5" ];
+
+export {
+    cMajorIonian,
+    cSharpMajorIonian,
+    dMajorIonian,
+    dSharpMajorIonian,
+    eMajorIonian,
+    fMajorIonian,
+    fSharpMajorIonian,
+    gMajorIonian,
+    gSharpMajorIonian,
+    aMajorIonian,
+    aSharpMajorIonian,
+    bMajorIonian,
+}
