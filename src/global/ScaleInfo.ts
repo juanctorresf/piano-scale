a60d27bc534c755462c2ac4ab167c5b9bd462f4b
Juan Torres <jtorres.devv@gmail.com>
2024-04-29 22:24:32 -0500

    Change some types of major and minor chords

--- src/global/ScaleInfo.ts
+++ src/global/ScaleInfo.ts
@@ -1,7 +1,7 @@
 const notes:string[] = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 const modality:string[] = ["Major", "Minor"];
-const typeMajors:string[] = [ "Ionian", "Bebop", "Bulgarian", "Hexatonic", "Pentatonic", "Persian", "Polymode" ];
-const typeMinors:string[] = [ "Harmonic", "Hungarian", "Melodic", "Natural", "Neapolitan", "Pentatonic", "Polymode", "Romanian" ];
+const typeMajors:string[] = [ "Ionian", /* "Bebop", "Bulgarian", "Hexatonic", "Pentatonic", "Persian", "Polymode" */ ];
+const typeMinors:string[] = [ /* "Harmonic", "Hungarian", "Melodic", */ "Natural",/*  "Neapolitan", "Pentatonic", "Polymode", "Romanian" */ ];
 
 export {
 	notes,
