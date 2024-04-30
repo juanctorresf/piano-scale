caef5b9e68231167649c1f5c6020a92dbceef66e
Juan Torres <jtorres.devv@gmail.com>
2024-04-30 00:23:29 -0500

    Add minor natural scale

---
+++ src/global/ScaleMinor.ts
@@ -0,0 +1,28 @@
+const cMinorNatural: string [] = [ "C4", "D4", "D#4", "F4", "G4", "G#4", "A#4", "C5" ];
+const cSharpMinorNatural: string [] = [ "C#4", "D#4", "E4", "F#4", "G#4", "A4", "B4", "C#5" ];
+const dMinorNatural: string [] = [ "D4", "E4", "F4", "G4", "A4", "A#4", "C5", "D5"];
+const dSharpMinorNatural: string [] = [ "D#4", "F4", "F#4", "G#4", "A#4", "B4", "C#5", "D#5" ];
+const eMinorNatural: string [] = [ "E4", "F#4", "G4", "A4", "B4", "C5", "D5", "E5" ];
+const fMinorNatural: string [] = [ "F4", "G4", "G#4", "A#4", "C5", "C#5", "D#5", "F5" ];
+const fSharpMinorNatural: string [] = [ "F#4", "G#4", "A4", "B4", "C#5", "D5", "E5", "F#5" ];
+const gMinorNatural: string [] = [ "G4", "A4", "A#4", "C5", "D5", "D#5", "F5", "G5" ];
+const gSharpMinorNatural: string [] = [ "G#4", "A#4", "B4", "C#5", "D#5", "E5", "F#5", "G#5" ];
+const aMinorNatural: string [] = [ "A4", "B4", "C5", "D5", "E5", "F5", "G5", "A5" ];
+const aSharpMinorNatural: string [] = ["A#4", "C5", "C#5", "D#5", "F5", "F#5", "G#5", "A#5" ];
+const bMinorNatural: string [] = [ "B4", "C#5", "D5", "E5", "F#5", "G5", "A5", "B5" ];
+
+
+export {
+    cMinorNatural,
+    cSharpMinorNatural,
+    dMinorNatural,
+    dSharpMinorNatural,
+    eMinorNatural,
+    fMinorNatural,
+    fSharpMinorNatural,
+    gMinorNatural,
+    gSharpMinorNatural,
+    aMinorNatural,
+    aSharpMinorNatural,
+    bMinorNatural,
+}
