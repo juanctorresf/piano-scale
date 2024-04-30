fa002b34a8a9a1d9714d916b0b5558287561834c
Juan Torres <jtorres.devv@gmail.com>
2024-04-29 22:25:03 -0500

    Fix code style

--- src/components/Piano/Piano.tsx
+++ src/components/Piano/Piano.tsx
@@ -1,13 +1,12 @@
 import { keys } from "../../global/KeyData"
 import { KeyPiano } from "./Key"
 
-
 export const Piano = () => {
 	return (
 		<div className="flex flex-row justify-center my-7">
 			{
-				keys.map( key => (
-					<KeyPiano key={ key } note={ key } />
+				keys.map(( key ) => (
+					<KeyPiano key={ key } noteKey={ key }/>
 				))
 			}
 		</div>
