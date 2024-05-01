abb27bb9f98f40815f89fcd61250d343d5e3a06a
Juan Torres <jtorres.devv@gmail.com>
2024-04-30 23:15:10 -0500

    Add dark theme

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,8 @@ export const Header = () => {
         <h1 
             className={ clsx(
                 "m-0 text-5xl text-center text-gray-10 pt-21 mb-9",
-                "md:text-6xl md:pt-45"
+                "md:text-6xl md:pt-45",
+                "dark: text-slate-100"
             )}
         >
             Piano Scale
