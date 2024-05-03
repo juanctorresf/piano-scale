f995c9e33dc32692eeaf5f7a78a6c2c306609e45
Juan Torres <jtorres.devv@gmail.com>
2024-05-02 22:19:22 -0500

    Add text shadow to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
     return (
         <h1 
             className={ clsx(
-                "m-0 text-5xl text-center text-gray-10 pt-21 mb-9",
+                "m-0 text-5xl text-center text-gray-10 pt-21 mb-9 text-shadow-header",
                 "md:text-6xl md:pt-45",
                 "dark:text-slate-100"
             )}
